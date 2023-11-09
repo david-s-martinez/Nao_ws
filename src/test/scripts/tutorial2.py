2b15302a8bcdb6157e4c34c1f16c3fc247382333
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2023-11-09 12:34:27 +0100

    feat: added threshold

--- src/test/scripts/tutorial2.py
+++ src/test/scripts/tutorial2.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python
 import rospy
-# from std_msgs.msg import String
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 
 def callback(data):
-
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-    cv2.imshow('Original Image',cv_image)
-    cv2.waitKey(1)
-    cv2.imshow('Greyscale_Image',cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY))
+    
+    # Display the original image
+    cv2.imshow('Original Image', cv_image)
+    
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Greyscale Image', gray_image)
+    
+    # Threshold the grayscale image to create a binary image
+    _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow('Binary Image', binary_image)
+
+    # Wait for a key press and close the windows if 'q' is pressed
     key = cv2.waitKey(1)
-    if key == 27:
-        #cv2.destroyAllWindows()
-        exit()
+    if key & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
-
     rospy.Subscriber("/nao_robot/camera/top/camera/image_raw", Image, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':

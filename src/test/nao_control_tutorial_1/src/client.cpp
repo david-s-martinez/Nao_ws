b0a573cf06f0aeb105e61b68a349ecd3f297d318
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2023-11-21 22:45:25 +0100

    feat: add mode select and angle interpol

--- src/test/nao_control_tutorial_1/src/client.cpp
+++ src/test/nao_control_tutorial_1/src/client.cpp
@@ -84,33 +84,53 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "nao_tutorial_control_1");
     Nao_control ic;
-
-    //Exercise 2
-    std::string joint_names[1] = {"HeadYaw"};
-    double anglesList[5] = {90.0, -90.0, 90.0, -90.0, 0.0};
-    double time_assigned[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
-    bool isAbsolute = true;
-
-    ic.callMoveJointsInterpolationService(joint_names, anglesList, time_assigned, isAbsolute);
-    ros::Duration(1.0).sleep();  // Sleep for 1 second (adjust as needed)
-    // Exercise 1 
-    for (int i = 0; i < 3; ++i)
+    while (true)
     {
-        // Call the service with different angles (-90.0, -45.0, 0.0)
-        std::string joint_name_1 = "RShoulderRoll";
-        std::string joint_name_2 = "RShoulderPitch";
-        double angle1 = -90.0 + i *30.0;  // Adjust the angle increment as needed
-        double angle2 = 0.0 + i *25.0;  // Adjust the angle increment as needed
-        double speed = 0.1;
+        char x; 
+        cout << "Choose the desire mode (AngleInterpolation or simple AnglesSetting). \n";
+        cout << "Press I for AngleInterpolation and A for AngleSetting: \n";
+        cin >> x; // Get user input from the keyboard
+        cout << "Your mode is: " << x << "\n"; // Display the input value 
+        if (x == 'I')
+        {
+            //Exercise 2
+            std::string joint_names[1] = {"LShoulderRoll"};
+            double anglesList[5] = {6.0, 12.0, 18.0, 24.0, 30.0};
+            double time_assigned[5] = {0.4, 0.8, 1.2, 1.6, 2.0};
+            bool isAbsolute = true;
 
-        // Call the service with the current joint, angle, and speed
-        ic.callMoveJointsService(joint_name_1, angle1, speed);
-        ros::Duration(1.0).sleep();  // Sleep for 2 seconds (adjust as needed)
-        ic.callMoveJointsService(joint_name_2, angle2, speed);
-        // Add a delay or other processing if needed
-        ros::Duration(1.0).sleep();  // Sleep for 2 seconds (adjust as needed)
+            ic.callMoveJointsInterpolationService(joint_names, anglesList, time_assigned, isAbsolute);
+            ros::Duration(1.0).sleep();  // Sleep for 1 second (adjust as needed)
+            ros::spinOnce();
+        }
+        else if (x == 'A')
+        {
+            // Exercise 1 
+            for (int i = 0; i < 3; ++i)
+            {
+                // Call the service with different angles (-90.0, -45.0, 0.0)
+                std::string joint_name_1 = "RShoulderRoll";
+                std::string joint_name_2 = "RShoulderPitch";
+                double angle1 = -90.0 + i *30.0;  // Adjust the angle increment as needed
+                double angle2 = 0.0 + i *25.0;  // Adjust the angle increment as needed
+                double speed = 0.5;
+
+                // Call the service with the current joint, angle, and speed
+                ic.callMoveJointsService(joint_name_1, angle1, speed);
+                // ros::Duration(1.0).sleep();  // Sleep for 2 seconds (adjust as needed)
+                // ic.callMoveJointsService(joint_name_2, angle2, speed);
+                // Add a delay or other processing if needed
+                // ros::Duration(1.0).sleep();  // Sleep for 2 seconds (adjust as needed)
+                ros::spinOnce();
+            }
+        }
+        else
+        {
+            cout << "Please input a correct method \n";
+        }
+
+        
     }
 
-    ros::spin();
     return 0;
 }
